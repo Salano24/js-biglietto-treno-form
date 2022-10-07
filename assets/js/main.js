e8ea33fa18577506432f3d53afe2fe7e32e5891d
Salano24 <salernomarco48@email.com>
2022-10-07 15:02:41 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+/* 
+Il programma dovrà chiedere all 'utente:
+il numero di chilometri che vuole percorrere
+e l 'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+  il prezzo del biglietto è definito in base ai km(0.21€ al km)
+va applicato uno sconto del 20 % per i minorenni
+va applicato uno sconto del 40 % per gli over 65.
+L 'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+*/
+const priceEl = document.getElementById('price')
+const pricePerKm = 0.21;
+let discount, ticketPrice;
+// 1. chiedere all'utente il numero di chilometri che vuole percorrere
+
+
+const km = Number(document.getElementById('km_da_percorrere'));
+// 2. chiedere all'utente l 'età del passeggero
+const passengerAge = Number(document.getElementById('fascia_eta'));
+
+//console.log(km, passengerAge);
+// 3. Sulla base di queste informazioni calcolare il prezzo totale del viaggio, secondo queste regole:
+// 3.1 il prezzo del biglietto è definito in base ai km(0.21€ al km)
+
+ticketPrice = km * pricePerKm;
+
+//console.log(ticketPrice);
+
+// 3.2 va applicato uno sconto del 20 % per i minorenni
+if (passengerAge < 18) {
+  // Applica sconto del 20%
+  discount = 0.2
+}
+// 3.3 va applicato uno sconto del 40 % per gli over 65.
+else if (passengerAge > 65) {
+  // applica sconto del 40%
+  discount = 0.4
+} else {
+  // no discount
+  discount = 0
+}
+const discountAmount = ticketPrice * discount;
+ticketPrice = ticketPrice - discountAmount
+  // mostriamo a schermo un messagio al posto del prezzo
+
+  priceEl.innerHTML = `€ ${ticketPrice.toFixed(2)}`
