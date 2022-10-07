1204179019a067e653e68220e0e6adceda9788a7
Salano24 <salernomarco48@email.com>
2022-10-07 16:22:42 +0200

    Struttura form senza abbellimenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,19 +10,15 @@ L 'output del prezzo finale va messo fuori in forma umana (con massimo due decim
 */
 const priceEl = document.getElementById('price')
 const pricePerKm = 0.21;
+const generaBtn = document.getElementById('genera');
 let discount, ticketPrice;
 // 1. chiedere all'utente il numero di chilometri che vuole percorrere
 
+  generaBtn.addEventListener('click', function(){
+    const passengerAge = Number (document.getElementById('fascia_eta'))
+    const km = Number (document.getElementById('km_da_percorrere'))
 
-const km = Number(document.getElementById('km_da_percorrere'));
-// 2. chiedere all'utente l 'età del passeggero
-const passengerAge = Number(document.getElementById('fascia_eta'));
-
-//console.log(km, passengerAge);
-// 3. Sulla base di queste informazioni calcolare il prezzo totale del viaggio, secondo queste regole:
-// 3.1 il prezzo del biglietto è definito in base ai km(0.21€ al km)
-
-ticketPrice = km * pricePerKm;
+    ticketPrice = km * pricePerKm;
 
 //console.log(ticketPrice);
 
@@ -41,6 +37,16 @@ else if (passengerAge > 65) {
 }
 const discountAmount = ticketPrice * discount;
 ticketPrice = ticketPrice - discountAmount
-  // mostriamo a schermo un messagio al posto del prezzo
 
+// Controllare se il valore di ticket price é NaN e se si mostrare una allerta
+if (isNaN(ticketPrice)) {
+  // mostriamo un alerta
+  alert('Non hai inserito correttamente i km da percorrere! Devi inserire un valore numerico.')
+  // mostriamo a schermo un messagio al posto del prezzo
+  priceEl.innerHTML = `Ops! c'é stato un problema! Riprova.`
+
+} else {
   priceEl.innerHTML = `€ ${ticketPrice.toFixed(2)}`
+}
+
+  })
